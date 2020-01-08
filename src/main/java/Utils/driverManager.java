149964257374dad01c1eb270c7d1ab3128f687d6
Swapneel Patil <swapneel.patil08@gmail.com>
2020-01-08 15:39:35 +0530

    First Working test

--- src/main/java/Utils/driverManager.java
+++ src/main/java/Utils/driverManager.java
@@ -1,26 +1,29 @@
 package Utils;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class driverManager {
-    public static AppiumDriver appiumDriver;
+    public static AppiumDriver<MobileElement> appiumDriver;
 
     public static void openDriver() throws Exception {
 
-        appiumDriver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), getCapabilities());
+        appiumDriver = new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), getCapabilities());
         appiumDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     private static DesiredCapabilities getCapabilities(){
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("appium-version", "1.15.1");
+        capabilities.setCapability("appium-version", "1.9.1");
         capabilities.setCapability("platformName", "android");
-        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("deviceName", "Andriod SDK built for x86");
         capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("UDID", "emulator-5554");
+        capabilities.setCapability("browserName", "Chrome");
 
         return capabilities;
     }
