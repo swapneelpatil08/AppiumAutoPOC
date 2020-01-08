cb9d4c336129d5d06a3e061663b3f02a3f237712
Swapneel Patil <swapneel.patil08@gmail.com>
2020-01-08 15:12:59 +0530

    Added driverManager and Test

--- src/main/java/Utils/driverManager.java
+++ src/main/java/Utils/driverManager.java
@@ -1,4 +1,32 @@
 package Utils;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class driverManager {
+    public static AppiumDriver appiumDriver;
+
+    public static void openDriver() throws Exception {
+
+        appiumDriver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), getCapabilities());
+        appiumDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    private static DesiredCapabilities getCapabilities(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("appium-version", "1.15.1");
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("platformVersion", "10");
+
+        return capabilities;
+    }
+
+    public static void closeDriver()
+    {
+        appiumDriver.close();
+    }
 }
--- src/main/java/tests/HomeTest.java
+++ src/main/java/tests/HomeTest.java
@@ -1,4 +1,13 @@
 package tests;
+import Utils.driverManager;
+import org.testng.annotations.Test;
 
 public class HomeTest {
+
+    @Test
+    public void OpenDriver() throws Exception {
+        driverManager.openDriver();
+        driverManager.closeDriver();
+    }
+
 }
